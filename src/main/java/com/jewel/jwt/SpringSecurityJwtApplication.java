ecb6063fdc1c78400ab438b6232457b765030f74
jewel73 <jewelranar420@gmail.com>
2021-06-13 20:55:45 +0600

    Spring Application with jwt security

---
+++ src/main/java/com/jewel/jwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,15 @@
+package com.jewel.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jewel/jwt/controller/HomeController.java
@@ -0,0 +1,52 @@
+package com.jewel.jwt.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jewel.jwt.config.JwtUtil;
+import com.jewel.jwt.model.AuthenticationRequest;
+import com.jewel.jwt.model.AuthenticationResponse;
+
+@RestController
+public class HomeController {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+	
+	@Autowired
+	UserDetailsService myUserDetailsService;
+	
+
+	@RequestMapping("/hello")
+	public String hello(HttpSession ssh) {
+		String id = ssh.getId();
+		return "Hello Programmer"+ id;
+	}
+	
+	@RequestMapping(value= "/authenticate", method= RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		String jwt = jwtTokenUtil.generateToken(userDetails);
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
+	}
+}
